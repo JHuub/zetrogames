912f3af8200df80c7f7dd00f12fadfde21b6540a
wwoa1 <108261176+wwoa1@users.noreply.github.com>
2023-03-23 07:31:33 -0700

    Basics App Descrip C++
    dec 1 of 1 for basics 1

---
+++ Basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "This is the (001) version of";
+  cout << ", version-1.";
+    cout << " Thsi version is the main version of sof111";
+  cout << ", origin-none.";
+    cout << "CNAME file is created";
+  cout << ", zetrogames.com.";
+    cout << "index file";
+  cout << ", version-1.";
+  return 0;
+}
