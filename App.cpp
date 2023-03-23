c46daf870311918e3534421b27fcabb57bbf69e2
wwoa1 <108261176+wwoa1@users.noreply.github.com>
2023-03-23 08:05:24 -0700

    App Data Numbers
    extra c++ file to use when identifying version history

---
+++ App.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << sqrt(1) << "\n";
+  cout << round(1) << "\n";
+  cout << log(90) << "\n";
+    cout << sqrt(2) << "\n";
+  cout << round(2) << "\n";
+  cout << log(91) << "\n";
+    cout << sqrt(3) << "\n";
+  cout << round(3) << "\n";
+  cout << log(92) << "\n";
+    cout << sqrt(4) << "\n";
+  cout << round(4) << "\n";
+  cout << log(93) << "\n";
+    cout << sqrt(5) << "\n";
+  cout << round(5) << "\n";
+  cout << log(94) << "\n";
+    cout << sqrt(6) << "\n";
+  cout << round(6) << "\n";
+  cout << log(95) << "\n";
+    cout << sqrt(7) << "\n";
+  cout << round(7) << "\n";
+  cout << log(96) << "\n";
+    cout << sqrt(8) << "\n";
+  cout << round(8) << "\n";
+  cout << log(97) << "\n";
+    cout << sqrt(9) << "\n";
+  cout << round(9) << "\n";
+  cout << log(98) << "\n";
+  return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 90; i++) {
+    cout << i << "\n";
+  }
+  return 0;
+}
+
