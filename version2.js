ce60803390c32371389b6a3900c12a20acaf8b0c
Jonas Hubbard <108261176+Jonaz98@users.noreply.github.com>
2023-08-18 10:05:24 +0900

    Create version2.js

---
+++ version2.js
@@ -0,0 +1,11 @@
+const d = new Date("2023-08-18");
+const nd = ("2023-08-18")
+const v = 1.2
+const c = ("Moderation update.")
+const md = ("Jonaz98")
+
+console.log("Day used - ",d);
+console.log("version number - ",v);
+console.log("this version is the - ", c);
+console.log("Last version was - ", nd);
+console.log("Update issued by - ", md)
