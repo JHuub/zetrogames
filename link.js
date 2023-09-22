509c6b5172c72bb0a7bd393cb86e7eb34fd4a68c
Jonas Hubbard <108261176+Jonaz98@users.noreply.github.com>
2023-09-22 17:32:50 +0900

    Add files via upload

---
+++ link.js
@@ -0,0 +1,173 @@
+const element1 = document.getElementById("storeupdate");
+element1.addEventListener("click", showele1);
+
+const element9 = document.getElementById("StoreUpdateMenu");
+element9.addEventListener("click", hideele1);
+
+const element10 = document.getElementById("tokensupdate");
+element10.addEventListener("click", showele2);
+
+const element11 = document.getElementById("ZetroTokensUpdateMenu");
+element11.addEventListener("click", hideele2);
+
+const element12 = document.getElementById("Zetroplus");
+element12.addEventListener("click", showele3);
+
+const element13 = document.getElementById("ZetroPlusUpdateMenu");
+element13.addEventListener("click", hideele3);
+
+const element14 = document.getElementById("ZagUpdate");
+element14.addEventListener("click", showele4);
+
+const element15 = document.getElementById("ZagUpdateMenu");
+element15.addEventListener("click", hideele4);
+
+const element16 = document.getElementById("ZagUpdateMenu");
+element15.addEventListener("click", hideele4);
+
+const element17 = document.getElementById("UserABtn");
+element17.addEventListener("click", showele5);
+
+const element18 = document.getElementById("UserAMenu");
+element18.addEventListener("click", hideele5);
+
+const element19 = document.getElementById("UserBBtn");
+element19.addEventListener("click", showele6);
+
+const element20 = document.getElementById("UserBMenu");
+element20.addEventListener("click", hideele6);
+
+const element21 = document.getElementById("UserCBtn");
+element21.addEventListener("click", showele7);
+
+const element22 = document.getElementById("UserCMenu");
+element22.addEventListener("click", hideele7);
+
+const element23 = document.getElementById("UserDBtn");
+element23.addEventListener("click", showele8);
+
+const element24 = document.getElementById("UserDMenu");
+element24.addEventListener("click", hideele8);
+
+//dpkcertifiedboyyounotus
+
+const element25 = document.getElementById("ContentABtn");
+element25.addEventListener("click", showele9);
+
+const element26 = document.getElementById("ContentA");
+element26.addEventListener("click", hideele9);
+
+const element27 = document.getElementById("ContentBBtn");
+element27.addEventListener("click", showele10);
+
+const element28 = document.getElementById("ContentB");
+element28.addEventListener("click", hideele10);
+
+const element29 = document.getElementById("ContentCBtn");
+element29.addEventListener("click", showele11);
+
+const element30 = document.getElementById("ContentC");
+element30.addEventListener("click", hideele11);
+
+const element31 = document.getElementById("ContentDBtn");
+element31.addEventListener("click", showele12);
+
+const element32 = document.getElementById("ContentD");
+element32.addEventListener("click", hideele12);
+
+function showele1() {
+    document.getElementById("StoreUpdateMenu").style.visibility = "visible";
+}
+
+function hideele1() {
+    document.getElementById("StoreUpdateMenu").style.visibility = "hidden";
+} 
+
+function showele2() {
+    document.getElementById("ZetroTokensUpdateMenu").style.visibility = "visible";
+}
+
+function hideele2() {
+    document.getElementById("ZetroTokensUpdateMenu").style.visibility = "hidden";
+}
+
+function showele3() {
+    document.getElementById("ZetroPlusUpdateMenu").style.visibility = "visible";
+}
+
+function hideele3() {
+    document.getElementById("ZetroPlusUpdateMenu").style.visibility = "hidden";
+}
+
+
+function showele4() {
+    document.getElementById("ZagUpdateMenu").style.visibility = "visible";
+}
+
+function hideele4() {
+    document.getElementById("ZagUpdateMenu").style.visibility = "hidden";
+}
+
+function showele5() {
+    document.getElementById("UserAMenu").style.visibility = "visible";
+}
+
+function hideele5() {
+    document.getElementById("UserAMenu").style.visibility = "hidden";
+}
+
+function showele6() {
+    document.getElementById("UserBMenu").style.visibility = "visible";
+}
+
+function hideele6() {
+    document.getElementById("UserBMenu").style.visibility = "hidden";
+}
+
+function showele7() {
+    document.getElementById("UserCMenu").style.visibility = "visible";
+}
+
+function hideele7() {
+    document.getElementById("UserCMenu").style.visibility = "hidden";
+}
+
+function showele8() {
+    document.getElementById("UserDMenu").style.visibility = "visible";
+}
+
+function hideele8() {
+    document.getElementById("UserDMenu").style.visibility = "hidden";
+}
+
+function showele9() {
+    document.getElementById("ContentA").style.visibility = "visible";
+}
+
+function hideele9() {
+    document.getElementById("ContentA").style.visibility = "hidden";
+}
+
+function showele10() {
+    document.getElementById("ContentB").style.visibility = "visible";
+}
+
+function hideele10() {
+    document.getElementById("ContentB").style.visibility = "hidden";
+}
+
+function showele11() {
+    document.getElementById("ContentC").style.visibility = "visible";
+}
+
+function hideele11() {
+    document.getElementById("ContentC").style.visibility = "hidden";
+}
+
+function showele12() {
+    document.getElementById("ContentD").style.visibility = "visible";
+}
+
+function hideele12() {
+    document.getElementById("ContentD").style.visibility = "hidden";
+}
